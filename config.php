e35382ecff6f671134110d1a1cce892a2ea07cfe
feng04050632 <feng040506@feng040506.com>
2023-10-23 16:33:38 +0800

    Flarum

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php return array (
+  'debug' => false,
+  'database' => 
+  array (
+    'driver' => 'mysql',
+    'host' => '127.0.0.1',
+    'port' => 3306,
+    'database' => 'Flarum',
+    'username' => 'root',
+    'password' => '123456',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => 'F_',
+    'strict' => false,
+    'engine' => 'InnoDB',
+    'prefix_indexes' => true,
+  ),
+  'url' => 'http://www.test.com',
+  'paths' => 
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+  'headers' => 
+  array (
+    'poweredByHeader' => true,
+    'referrerPolicy' => 'same-origin',
+  ),
+);
